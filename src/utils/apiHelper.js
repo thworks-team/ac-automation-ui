8881026c9873245b353a8930051f0fd05309e478
Selvaraj <selva@thworks.org>
2023-06-22 12:19:15 +0530

    Revert "changes in axios"
    This reverts commit 36b3cb828f434552bbc4e8bb2e99dcb3fe2c8a1b.

--- src/utils/apiHelper.js
+++ src/utils/apiHelper.js
@@ -1,58 +1,35 @@
 import axios from 'axios';
 
-const baseURL =  'http://api.sensor.thworks.org/api/v1';
+const axiosClient = axios.create();
+
+axiosClient.defaults.baseURL = 'http://api.sensor.thworks.org/api/v1';
+
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjoiNjQ3NjNmZjRmMzVhZjM2ODhmNmJjNmU2IiwiaWF0IjoxNjg1NDcxMjc5LCJleHAiOjE2ODU1MjUyNzl9.7jH__nsQ1EZgYieUl79VXjVoP0vKtaFpJ8joKm0WXyY';
+
+axiosClient.defaults.headers = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+  Authorization : `Bearer ${token}`,
+  'Access-Control-Allow-Origin':'*'
+};
 
 //All request will wait 2 seconds before timeout
 // axiosClient.defaults.timeout = 2000;
 
-// axiosClient.withCredentials = true;
+axiosClient.defaults.withCredentials = true;
 
 export function getRequest(URL) {
-  return axios({
-    method: 'get',
-    url: `${baseURL}${URL}`,
-    headers: {
-      Authorization:
-        `Bearer ${token}`
-    },
-  });
+  return axiosClient.get(`${URL}`).then(response => response);
 }
 
 export function postRequest(URL, payload) {
-  return axios({
-    method: 'post',
-    url: `${baseURL}${URL}`,
-    headers: {
-      Authorization:
-        `Bearer ${token}`
-    },
-    data: payload
-  });
-  // return axiosClient.post(`${URL}`, payload).then(response => response);
+  return axiosClient.post(`${URL}`, payload).then(response => response);
 }
 
 export function patchRequest(URL, payload) {
-  return axios({
-    method: 'get',
-    url: `${baseURL}${URL}`,
-    headers: {
-      Authorization:
-        `Bearer ${token}`
-    },
-    data: payload
-  });
-  // return axiosClient.patch(`${URL}`, payload).then(response => response);
+  return axiosClient.patch(`${URL}`, payload).then(response => response);
 }
 
 export function deleteRequest(URL) {
-  return axios({
-    method: 'get',
-    url: `${baseURL}${URL}`,
-    headers: {
-      Authorization:
-        `Bearer ${token}`
-    },
-  });
-  // return axiosClient.delete(`${URL}`).then(response => response);
+  return axiosClient.delete(`${URL}`).then(response => response);
 }
